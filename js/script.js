dd54788a658e86b9e036f84d541f803c08ca1cbe
parksangseok <park0157@naver.com>
2025-11-20 11:14:39 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 $(function () {
 
   //헤더 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#headerWrap').addClass('scroll');
+    } else {
+      $('#headerWrap').removeClass('scroll');
+    }
+  });
+
+  $(window).load(function () {
+    $('#headerWrap').addClass('active');
+    setInterval(function () {
+      $('#headerWrap').removeClass('active');
+      setTimeout(function () {
+        $('#headerWrap').addClass('active');
+      }, 20);
+    }, 5000);
+  });
+
   $('#headerWrap .depth01 li h2 a').mouseover(function () {
     $('#headerWrap .depth01 li h2 a').removeClass('active');
     $(this).addClass('active');
@@ -40,70 +58,37 @@ $(function () {
     $('body').removeClass('of');
   });
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-      $('#headerWrap').addClass('scroll');
-    } else {
-      $('#headerWrap').removeClass('scroll');
+  $('#mobileMenuBox .mobileDepth01 > li').first().find('.mobileDepth02').show();
+  $('#mobileMenuBox .mobileDepth01 > li').first().find('h2 > a').addClass('active');
+  $('#mobileMenuBox .mobileDepth01 li h2 a').click(function () {
+    $('#mobileMenuBox .mobileDepth01 > li > h2 > a').removeClass('active');
+    $(this).addClass('active');
+    $('#mobileMenuBox .mobileDepth02').hide();
+    $(this).parents('li').find('.mobileDepth02').show();
+  });
+
+  $('#mobileMenuBox .mobileDepth02 li h3 a').click(function () {
+    if ($(this).parent().parent().find('.mobileDepth03').length) {
+      if ($(this).parent().hasClass('active')) {
+        $(this).parent().parent().find('.mobileDepth03').slideUp();
+        $(this).parent().removeClass('active');
+      } else {
+        $('#mobileMenuBox .mobileDepth02 li').find('.mobileDepth03').slideUp();
+        $(this).parent().parent().find('.mobileDepth03').slideDown();
+        $('#mobileMenuBox .mobileDepth02 li h3').removeClass('active');
+        $(this).parent().addClass('active');
+      }
+      return false;
     }
   });
+  $('#mobileMenuBox .mobileDepth02 li:has(ul)').children('h3').addClass('depth03');
 
 
 
 
 
-  //팝업
-  // $(window).load(function () {
-  //   $('.popupWrap').addClass('active');
-  //   if (($('.popupWrap').hasClass('active'))) {
-  //     $('body').addClass('fixed');
-  //   }
-  // });
-
-  $(".popupBox .slick").slick({
-    autoplay: true,
-    arrows: true,
-    dots: false,
-    accessibility: false,
-    draggable: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    zIndex: 1000,
-    pauseOnHover: false,
-    autoplaySpeed: 5000,
-    speed: 1500,
-    prevArrow: $('.popupBox .prev'),
-    nextArrow: $('.popupBox .next'),
-  });
-
-  //헤더 
-  $(window).load(function () {
-
-    $('#headerWrap').addClass('active');
-
-    setInterval(function () {
-      $('#headerWrap').removeClass('active');
-
-      setTimeout(function () {
-        $('#headerWrap').addClass('active');
-      }, 20);
-
-    }, 5000);
-  });
 
 
-
-  $('#headerWrap .menuBox .menuList > li:has(ul)').children('h2').addClass('active');
-
-  //모바일 메뉴
-  $('#headerWrap .mobileMenuBtn').click(function () {
-    $('#headerWrap .mobileMenuWrap').addClass('active');
-  });
-
-  $('#headerWrap .mobileCloseBtn').click(function () {
-    $('#headerWrap .mobileMenuWrap').removeClass('active');
-  });
 
   //스크롤 시 top 버튼 fadeIn, fadeOut
   $(window).scroll(function () {
