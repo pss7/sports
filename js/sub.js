2d97a279369e1aa26f45b93a4a3d880999375287
parksangseok <park0157@naver.com>
2025-12-08 16:20:38 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -39,139 +39,66 @@ $(function () {
 
   });
 
-  //수상 슬라이드 영역 
-  $(".newsBox .slick").slick({
-    autoplay: false,
-    arrows: true,
-    dots: true,
-    accessibility: false,
-    draggable: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    zIndex: 1000,
-    pauseOnHover: false,
-    autoplaySpeed: 5000,
-    speed: 1500,
-    prevArrow: $('.newsBox .controlBox .prev'),
-    nextArrow: $('.newsBox .controlBox .next'),
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
+  //첨부파일
+  $("#fileInput").change(function () {
+    $("#fileNameText").text(this.files[0] ? this.files[0].name : "선택된 파일이 없습니다.");
   });
 
-  //비전 탭
-  $('.visionBox .coreValuesList_0602 > li a').click(function () {
-
-    if ($(this).parent().hasClass('active')) {
-      $('.visionBox .coreValuesTextBox_0604').slideUp();
-      $('.visionBox .coreValuesList_0602 > li').removeClass('active');
-    } else {
-      $('.visionBox .coreValuesTextBox_0604').hide();
-      $('.visionBox .coreValuesList_0602 > li').removeClass('active');
-      $(this).parent().addClass('active');
-      $(this).parent().find('.coreValuesTextBox_0604').slideDown();
-    }
-
-    return false;
-
+  $(".fileDelBtn").click(function () {
+    $("#fileInput").val('');
+    $("#fileNameText").text("선택된 파일이 없습니다.");
   });
 
-  //입-출금 신청
-  $('.applicationBox .contentBox').hide();
-  $('.applicationBox .contentBox').first().show();
+// 개별 선택
+$('.fileImgList li .fileBtn').click(function () {
+  $(this).toggleClass('active');
 
-  $('.applicationBox .tabBox .tab').click(function () {
+  let total    = $('.fileImgList .fileBtn').length;
+  let selected = $('.fileImgList .fileBtn.active').length;
 
-    $('.applicationBox .tabBox .tab').removeClass('active');
-    $(this).addClass('active');
+  if (total === selected && total > 0) {
+    $('.checkAllBtn').addClass('active').text('전체 해제');
+  } else {
+    $('.checkAllBtn').removeClass('active').text('전체 선택');
+  }
+});
 
-    let idx = $(this).index();
+// 전체 선택
+$('.checkAllBtn').click(function () {
+  let total    = $('.fileImgList .fileBtn').length;
+  let selected = $('.fileImgList .fileBtn.active').length;
 
-    $('.applicationBox .contentBox').hide();
-    $('.applicationBox .contentBox').eq(idx).show();
+  if (total === selected && total > 0) {
+    $('.fileImgList .fileBtn').removeClass('active');
+    $(this).removeClass('active').text('전체 선택');
+  } else {
+    $('.fileImgList .fileBtn').addClass('active');
+    $(this).addClass('active').text('전체 해제');
+  }
+});
 
-  });
 
-  $('.applicationBox .inquiryBtn').click(function () {
-    $('.issuedModalWrap').addClass('active');
-  });
 
-  $('.issuedModalWrap .modalCloseBtn').click(function () {
-    $('.issuedModalWrap').removeClass('active');
-  });
 
-  //자주 묻는 질문 
-  $('.questionBox .questionList li a').click(function () {
-    if ($(this).hasClass('active')) {
-      $('.questionBox .questionList li a').removeClass('active');
-      $('.questionBox .questionList li a').next().slideUp();
 
-    } else {
-      $('.questionBox .questionList li a').removeClass('active');
-      $('.questionBox .questionList li a').next().slideUp();
-      $(this).addClass('active');
-      $(this).next().slideDown();
-    }
-    return false
-  })
 
-  //모달
-  $('.detail_btn02').click(function () {
-    $('.productModalWrap02').addClass('active');
-  })
-  $('.productModalCloseBtn02').click(function () {
-    $('.productModalWrap02').removeClass('active');
-  })
 
-  $('.detail_btn01').click(function () {
-    $('.productModalWrap01').addClass('active');
-  })
-  $('.productModalCloseBtn01').click(function () {
-    $('.productModalWrap01').removeClass('active');
-  })
 
-  //이벤트 슬라이드 영역 
-  $(".eventListBox .slick").slick({
-    autoplay: false,
-    arrows: true,
-    dots: true,
-    accessibility: false,
-    draggable: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    zIndex: 1000,
-    pauseOnHover: false,
-    autoplaySpeed: 5000,
-    speed: 1500,
-    prevArrow: $('.eventListBox .comControl .prev'),
-    nextArrow: $('.eventListBox .comControl .next'),
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
