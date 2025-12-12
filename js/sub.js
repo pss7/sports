16109708d4d13ac9d472495aea729b6cd599efbb
parksangseok <park0157@naver.com>
2025-12-12 13:05:06 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -4,6 +4,7 @@ $(function () {
   //상단
   $(window).load(function () {
     $('.subVisualBox .visualBox').addClass('active');
+    // $('.visionBox .topImgBox').addClass('active');
   });
 
   //로그인 탭
@@ -49,36 +50,50 @@ $(function () {
     $("#fileNameText").text("선택된 파일이 없습니다.");
   });
 
-// 개별 선택
-$('.fileImgList li .fileBtn').click(function () {
-  $(this).toggleClass('active');
+  // 개별 선택
+  $('.fileImgList li .fileBtn').click(function () {
+    $(this).toggleClass('active');
 
-  let total    = $('.fileImgList .fileBtn').length;
-  let selected = $('.fileImgList .fileBtn.active').length;
+    let total = $('.fileImgList .fileBtn').length;
+    let selected = $('.fileImgList .fileBtn.active').length;
 
-  if (total === selected && total > 0) {
-    $('.checkAllBtn').addClass('active').text('전체 해제');
-  } else {
-    $('.checkAllBtn').removeClass('active').text('전체 선택');
-  }
-});
+    if (total === selected && total > 0) {
+      $('.checkAllBtn').addClass('active').text('전체 해제');
+    } else {
+      $('.checkAllBtn').removeClass('active').text('전체 선택');
+    }
+  });
 
-// 전체 선택
-$('.checkAllBtn').click(function () {
-  let total    = $('.fileImgList .fileBtn').length;
-  let selected = $('.fileImgList .fileBtn.active').length;
+  // 전체 선택
+  $('.checkAllBtn').click(function () {
+    let total = $('.fileImgList .fileBtn').length;
+    let selected = $('.fileImgList .fileBtn.active').length;
 
-  if (total === selected && total > 0) {
-    $('.fileImgList .fileBtn').removeClass('active');
-    $(this).removeClass('active').text('전체 선택');
-  } else {
-    $('.fileImgList .fileBtn').addClass('active');
-    $(this).addClass('active').text('전체 해제');
-  }
-});
+    if (total === selected && total > 0) {
+      $('.fileImgList .fileBtn').removeClass('active');
+      $(this).removeClass('active').text('전체 선택');
+    } else {
+      $('.fileImgList .fileBtn').addClass('active');
+      $(this).addClass('active').text('전체 해제');
+    }
+  });
 
+  //스크롤 시 해당 영역 active 클래스 적용
+  $(window).scroll(function () {
 
+    const scrollPos = $(window).scrollTop();
+    const winH = $(window).height();
 
+    $('.scrollElement').each(function () {
+      let elementOffset = $(this).offset().top;
+      if (scrollPos > elementOffset - winH / 1.2) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+
+  });
 
 
 
